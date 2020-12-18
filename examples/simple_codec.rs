a638dfb94127a8b010c7d39a6667a1a123150414
aeryz <abdullaheryz@protonmail.com>
2020-12-18 20:16:32 +0300

    experimenting with a simple codec

---
+++ examples/simple_codec.rs
@@ -0,0 +1,98 @@
+use bytes::buf::{Buf, BufMut};
+use bytes::BytesMut;
+use rand::Rng;
+use serde::de::DeserializeOwned;
+use std::collections::VecDeque;
+use std::convert::TryInto;
+static HEADER_LEN: usize = 8;
+
+#[derive(Debug)]
+enum State {
+    OnHeader,
+    OnBody(usize),
+}
+
+struct SimpleCodec<T> {
+    bytes: BytesMut,
+    parsed_buf: VecDeque<Result<T, ()>>,
+
+    state: State,
+}
+
+impl<T> SimpleCodec<T>
+where
+    T: DeserializeOwned,
+{
+    pub fn new() -> Self {
+        Self {
+            bytes: BytesMut::new(),
+            parsed_buf: VecDeque::new(),
+            state: State::OnHeader,
+        }
+    }
+
+    fn parse_header(&mut self) -> Option<()> {
+        if self.bytes.len() < HEADER_LEN {
+            return None;
+        }
+        self.state = State::OnBody(usize::from_le_bytes(
+            (&self.bytes.as_ref()[0..HEADER_LEN]).try_into().unwrap(),
+        ));
+        println!("{:?}", self.state);
+        self.parse_body()
+    }
+
+    fn parse_body(&mut self) -> Option<()> {
+        if let State::OnBody(len) = self.state {
+            if self.bytes.len() < len + HEADER_LEN {
+                return None;
+            }
+            self.parsed_buf.push_back(
+                serde_json::from_slice::<T>(&self.bytes.as_ref()[HEADER_LEN..HEADER_LEN + len])
+                    .map_err(|_| ()),
+            );
+            self.state = State::OnHeader;
+            self.bytes.advance(HEADER_LEN + len);
+            return Some(());
+        }
+        unreachable!();
+    }
+
+    pub fn extend(&mut self, data: &[u8]) {
+        self.bytes.put(data);
+        loop {
+            if let None = match self.state {
+                State::OnHeader => self.parse_header(),
+                State::OnBody(_) => self.parse_body(),
+            } {
+                break;
+            }
+        }
+    }
+
+    pub fn drain(&mut self) -> Option<Result<T, ()>> {
+        self.parsed_buf.pop_front()
+    }
+}
+
+#[derive(Debug, serde::Deserialize, serde::Serialize)]
+struct Data {
+    a: i32,
+}
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let mut codec: SimpleCodec<Data> = SimpleCodec::new();
+    let mut data: Vec<u8> = Vec::new();
+    for _ in 0..20 {
+        let a = Data { a: rng.gen() };
+        let bytes = serde_json::to_vec(&a).unwrap();
+        data.extend(&bytes.len().to_le_bytes());
+        data.extend(bytes.as_slice());
+    }
+    codec.extend(data.as_slice());
+
+    while let Some(codec) = codec.drain() {
+        println!("{:?}", codec);
+    }
+}
