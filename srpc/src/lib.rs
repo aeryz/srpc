de45f9faff2c8fa5be0a35430e770763c02310d4
aeryz <abdullaheryz@protonmail.com>
2020-12-21 20:42:58 +0300

    refactor(lib): remove utils, refactor lib exports

--- srpc/src/lib.rs
+++ srpc/src/lib.rs
@@ -2,25 +2,7 @@ pub mod client;
 pub mod json_rpc;
 pub mod server;
 pub mod transport;
-pub mod utils;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-pub use client::*;
 pub use srpc_macros::*;
-
-use std::sync::Arc;
-use tokio::io::AsyncWrite;
-use tokio::sync::Mutex;
-pub struct Data<W: AsyncWrite + Unpin> {
-    stream: Arc<Mutex<W>>,
-    res: Vec<u8>,
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
