13aec91e533b836cba64b26bd329ebe3d0d64d16
aeryz <abdullaheryz@protonmail.com>
2021-01-05 08:26:48 +0300

    fix(examples): improve client example

--- examples/example_client.rs
+++ examples/example_client.rs
@@ -18,10 +18,7 @@ trait StrService {
 async fn main() {
     env_logger::init();
     let transporter = Arc::new(Transport::new());
-    let client = Client::new(
-        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080),
-        transporter.clone(),
-    );
+    let client = Client::new(([127, 0, 0, 1], 8080).into(), transporter.clone());
 
     for i in 0..100 {
         let _ = StrService::set_data(&client, i % 2 == 0).await;
